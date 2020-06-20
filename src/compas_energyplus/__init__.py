aaa10c22e678a00ddcc8291412f1448c0baf5964
Tomas Mendez Echenagucia <tmendeze@uw.edu>
2020-06-20 12:31:29 -0700

    nothing

--- src/compas_energyplus/__init__.py
+++ src/compas_energyplus/__init__.py
@@ -19,7 +19,7 @@ import sys
 
 
 __author__ = ["Tomas Mendez Echenagucia"]
-__copyright__ = "Design Machine Group - University of Washington"
+__copyright__ = "2020, Design Machine Group - University of Washington"
 __license__ = "MIT License"
 __email__ = "tmendeze@uw.edu"
 __version__ = "0.1.0"
